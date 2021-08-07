3aa9c6386b32b22dda45fddb794bf05c54b606b6
lopezsergio29 <sdlopez2901@gmail.com>
2021-08-06 22:16:21 -0600

    Chingadera

--- app.py
+++ app.py
@@ -14,7 +14,7 @@ app.secret_key = "ILoveFishing"
 
 @app.route("/")
 def index():
-    return render_template("newloan.html")
+    return render_template("index.html")
 
 
 if __name__ == "__main__":
