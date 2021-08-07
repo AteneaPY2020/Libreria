03cc7eecef6671fdb720f43e2e98ec5d62d95c0f
Jakelin Rivera <60167444+Jakelin-prog@users.noreply.github.com>
2021-08-07 11:45:13 -0600

    añadir libros y ver catalogo ya funcionando

---
+++ libros.py
@@ -0,0 +1,36 @@
+from flask import Blueprint, render_template, request, redirect, session
+from logic import Logic
+from librosObj import librosObj
+from librosLogic import librosLogic
+
+libros = Blueprint(
+    "libros", __name__, template_folder="Templates", static_folder="static"
+)
+@libros.route("/book")
+def book():
+    return render_template("book.html")
+
+@libros.route("/catalog", methods=["GET", "POST"])
+def catalog():
+    if request.method == "GET":
+        logic = librosLogic
+        libros = logic().getAllLibros()
+    return render_template(
+            "catalog.html", data = libros
+        )
+@libros.route("/bookinsert", methods=["GET", "POST"])
+def bookinsert():
+    if request.method == "POST":
+        logic = librosLogic()
+        titulo= (request.form["titulo"])
+        autor= (request.form["autor"])
+        edicion = (request.form["edicion"])
+        editorial= (request.form["editorial"])
+        correlativo= (request.form["correlativo"])
+        pais= (request.form["pais"])
+        anno= (request.form["anno"])
+        disponibles= int((request.form["disponibles"]))
+        logic.insertLibro (titulo,autor,edicion, editorial, correlativo, pais, anno, disponibles, 1 )
+    return render_template(
+            "book.html", message = "Insertado correctamente"
+        )
